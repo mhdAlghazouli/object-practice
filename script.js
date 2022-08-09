646685b5e31d5245ce42cae9c69921abb9f8bb2b
mhd1995 <35552987+mhd1995@users.noreply.github.com>
2022-08-09 12:45:58 -0400

    added a script.js

---
+++ script.js
@@ -0,0 +1,59 @@
+// 1. Write a JavaScript program to list the properties of a JavaScript object.
+
+// const obj1 = {
+//   firstName : `Moe`,
+//   lastName : `Algahzouli`,
+//   age : 27
+// };
+
+// console.log(Object.keys(obj1));
+
+// // obj1 Output: name,sclass,rollno
+
+
+
+
+
+
+
+// Write a JavaScript program to delete the firstName property from the following object. Also print the object before or after deleting the property.
+
+// const obj2 = {
+//   firstName : `Moe`,
+//   lastName : `Algahzouli`,
+//   age : 27
+// };
+// delete obj2.firstName;
+
+// console.log(obj2);
+
+
+
+
+
+
+
+
+// Write a JavaScript program to get the length of a JavaScript object.
+
+// const obj3 = {
+//   firstName : `Moe`,
+//   lastName : `Algahzouli`,
+//   age : 27,
+//   major : `Software Engineer`
+// };
+
+// const size = Object.keys(obj3).length;
+
+// console.log(size);
+
+
+
+
+
+
+
+
+
+
+
